d150caa1bde904b81f4bd05fa0044da12e47d855
jebin <jebin@1tool.tech>
2021-10-15 00:16:48 +0530

    commented in main.go

--- main.go
+++ main.go
@@ -14,7 +14,8 @@ import (
 	. "github.com/mlabouardy/komiser/handlers/gcp"
 	. "github.com/mlabouardy/komiser/handlers/ovh"
 	. "github.com/mlabouardy/komiser/services/cache"
-	. "github.com/mlabouardy/komiser/services/ini"
+
+	//. "github.com/mlabouardy/komiser/services/ini"
 	"github.com/urfave/cli"
 )
 
